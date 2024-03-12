1c6f7602ed689d6200850cb9a7fc0b4929b4419c
Nitinisd007 <nitinen1019@gmail.com>
2024-03-12 22:47:54 +0530

    added start pattern file

---
+++ star pattern.py
@@ -0,0 +1,58 @@
+#print(eval(input('Enter The Equation')))
+# count=1
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(count,end=' ')
+#         count+=1
+#     print()
+
+
+## Square Pattern
+# for i in range(5):
+#    for j in range(5):
+#             print('*',end='  ')
+#    print()
+
+# Increasing Triangle
+# for i in range(5):
+#    for j in range(i+1): #column=row+1
+#             print('*',end='')
+#    print()
+
+# Increasing Triangle using if else
+# for i in range(1,5):
+#    for j in range(1,5):
+#        if j<=i:
+#          print('*',end='')
+#        else:
+#          print(" ",end='')
+# print( )
+   ## Decreasing Triangle
+# for i in range(5):
+#     for j in range(i,5):
+#              print('*',end='  ')
+#     print()
+ ## Right Sided Triangle
+
+# for i in range(5):
+#  for j in range(i,5):
+#     print(' ',end='  ')
+#     for j in range(i+1):
+#         print('*',end='  ')
+#     print()
+##### Increasing Triangle LHS in other way
+# for i in range(5):
+#     for j in range(5):
+#         if j<=i:
+#             print("*",end='')
+        
+#     print()
+
+##### Decreasing Triangle RHS in other way
+# for i in range(1,5):
+#     for j in range(1,5):
+#         if j>=5-i:
+#             print("*",end='')
+#         else:
+#             print(" ",end='') 
+#     print()
